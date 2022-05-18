3288488ec765b048a5f08d787d81402606acb66b
Thales Alves <thalesalves758@gmail.com>
2022-05-17 23:02:41 -0300

    Criar componente ChooseSession

---
+++ src/components/ChooseSession.js
@@ -0,0 +1,33 @@
+import Main from "./Main";
+import Title from "./Title";
+import Sessions from "./Sessions";
+import Session from "./Session";
+import Footer from "./Footer";
+import ReservationInfo from "./ReservationInfo";
+
+import showtimes from "../data/showtimes";
+
+export default function ChooseSession() {
+  return (
+    <>
+      <Main>
+        <Title>
+          Selecione o horário
+        </Title>
+        <Sessions>
+          { showtimes.days.map((day, index) => (
+            <Session
+              key={index}
+              weekday={day.weekday}
+              date={day.date}
+              showtimes={day.showtimes}
+            />
+          )) }
+        </Sessions>
+      </Main>
+      <Footer>
+        <ReservationInfo image={showtimes.posterURL} title={showtimes.title} />
+      </Footer>
+    </>
+  );
+}
