d41384716eea2a37f13ce04d915d4d6925d5fbf7
Thales Alves <thalesalves758@gmail.com>
2022-05-18 16:52:44 -0300

    Renderizar componente ChooseSeat para desenvolvimento

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import Header from "./Header";
 import ChooseMovie from "./ChooseMovie";
 import ChooseSession from "./ChooseSession";
+import ChooseSeat from "./ChooseSeat";
 
 export default function App() {
   return (
     <>
       <Header />
       {/* <ChooseMovie /> */}
-      <ChooseSession />
+      {/* <ChooseSession /> */}
+      <ChooseSeat />
     </>
   );
 }
