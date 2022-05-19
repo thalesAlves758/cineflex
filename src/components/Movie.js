eecef8746638d390ae82cacdb3cb8f6bd2077b80
Thales Alves <thalesalves758@gmail.com>
2022-05-18 23:30:32 -0300

    Realizar uma renderizacao com condicao

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,27 @@
 import { Link } from 'react-router-dom';
 
+import RenderIf from './utilities/RenderIf';
+
+function MovieBox({ image, title, small = false }) {
+  return (
+    <div className={`movie ${small ? 'small' : ''}`}>
+      <img src={image} alt={title} />
+    </div>
+  );
+}
+
 export default function Movie ({ id = null, image, title, small = false }) {
   return (
-    <Link to={`/sessoes/${id}`}>
-      <div className={`movie ${small ? 'small' : ''}`}>
-        <img src={image} alt={title} />
-      </div>
-    </Link>
+    <>
+      <RenderIf isTrue={id}>
+        <Link to={`/sessoes/${id}`}>
+          <MovieBox image={image} title={title} small={small} />
+        </Link>
+      </RenderIf>
+
+      <RenderIf isTrue={!id}>
+        <MovieBox image={image} title={title} small={small} />
+      </RenderIf>
+    </>
   );
 }
