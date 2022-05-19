c5bf8384a82c581034080f4aa319299fb9e3406f
Thales Alves <thalesalves758@gmail.com>
2022-05-18 23:25:57 -0300

    Implementar click em cada filme para rota /sessoes/:idFilme

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function Movie ({ id = null, image, title, small = false }) {
   return (
-    <div className={`movie ${small ? 'small' : ''}`}>
-      <img src={image} alt={title} />
-    </div>
+    <Link to={`/sessoes/${id}`}>
+      <div className={`movie ${small ? 'small' : ''}`}>
+        <img src={image} alt={title} />
+      </div>
+    </Link>
   );
 }
