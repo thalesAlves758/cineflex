5bc660b88ae3e2bb8db9fee23b637b80af0f253b
Thales Alves <thalesalves758@gmail.com>
2022-05-19 00:34:52 -0300

    Mostrar alert caso clicar num assento indisponivel

--- src/components/Seat.js
+++ src/components/Seat.js
@@ -6,15 +6,18 @@ export default function Seat({ name = null, isAvailable, selectedSeat = false })
   const [selected, setSelected] = useState(selectedSeat);
 
   function handleClick() {
+    if(!isAvailable) {
+      alert("Esse assento não está disponível");
+      return;
+    }
+
     setSelected(!selected);
   }
-
-  const isClickable = () => isAvailable && name;
 
   const addPadLeft = str => str ? str.padStart(TWO, '0') : null;
 
   return (
-    <div onClick={isClickable() ? handleClick : null} className={`seat ${!isAvailable ? 'unavailable' : ''} ${selected ? 'selected' : ''}`}>
+    <div onClick={name !== null ? handleClick : null} className={`seat ${!isAvailable ? 'unavailable' : ''} ${selected ? 'selected' : ''}`}>
       { addPadLeft(name) }
     </div>
   );
