b2e797aa87103ccd8068843d5681cad599cae17e
Thales Alves <thalesalves758@gmail.com>
2022-05-18 23:22:12 -0300

    Buscar filmes na api e renderiza-los

--- src/components/ChooseMovie.js
+++ src/components/ChooseMovie.js
@@ -1,11 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 import Main from "./Main";
 import Movie from "./Movie";
 import Movies from "./Movies";
 import Title from "./Title";
 
-import movies from "../data/movies";
-
 export default function ChooseMovie() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://mock-api.driven.com.br/api/v5/cineflex/movies')
+      .then(({ data }) => {
+        setMovies([...data]);
+      });
+  }, []);
+
   return (
     <Main>
       <Title>
