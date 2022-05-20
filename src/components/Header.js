bd25a7661cd4c77431b35debbda43ab107b26e3f
Thales Alves <thalesalves758@gmail.com>
2022-05-20 15:24:12 -0300

    Adicionar um botao no header da pagina

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
+import Button from './shared/Button';
 
 export default function Header() {
   return (
     <header>
       <Link to={'/'}>
+        <Button>
+          Voltar
+        </Button>
         <div className="logo">
           CINEFLEX
         </div>
