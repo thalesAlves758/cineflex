2f573ceb502dbc3dc894bc354b9bdc40b541acba
Thales Alves <thalesalves758@gmail.com>
2022-05-20 15:17:06 -0300

    Adicionar link para pagina home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
     <header>
-      CINEFLEX
+      <Link to={'/'}>
+        <div className="logo">
+          CINEFLEX
+        </div>
+      </Link>
     </header>
   );
 }
