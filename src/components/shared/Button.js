c60f501b3dd295a87aca84019469a7dda3e3003b
Thales Alves <thalesalves758@gmail.com>
2022-05-20 16:13:05 -0300

    Tornar click no botao opcional

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -1,4 +1,4 @@
-export default function Button({ children, onClick }) {
+export default function Button({ children, onClick = null}) {
   return (
     <button onClick={onClick} className="btn-primary">
       { children }
