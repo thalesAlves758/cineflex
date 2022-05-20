d2a3a2f85c0d0e57ff41099ecd9ab81033b51ff8
Thales Alves <thalesalves758@gmail.com>
2022-05-20 16:09:36 -0300

    Renderizar botao voltar apenas se nao estiver na pagina home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,21 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import Button from './shared/Button';
+import RenderIf from './utilities/RenderIf';
 
 const MINUS_ONE = -1;
 
 export default function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <header>
       <Link to={'/'}>
-        <Button onClick={() => navigate(MINUS_ONE, { replace: true })}>
-          Voltar
-        </Button>
+        <RenderIf isTrue={location.pathname !== '/'}>
+          <Button onClick={() => navigate(MINUS_ONE)}>
+            Voltar
+          </Button>
+        </RenderIf>
         <div className="logo">
           CINEFLEX
         </div>
