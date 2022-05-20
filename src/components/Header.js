9c89a81c12243d8eba15731f115da59128fa55e0
Thales Alves <thalesalves758@gmail.com>
2022-05-20 15:33:54 -0300

    Adicionar navegacao para pagina anterior

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Button from './shared/Button';
 
+const MINUS_ONE = -1;
+
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
     <header>
       <Link to={'/'}>
-        <Button>
+        <Button onClick={() => navigate(MINUS_ONE, { replace: true })}>
           Voltar
         </Button>
         <div className="logo">
